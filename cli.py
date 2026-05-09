beb0a5badcd194b990c84b6103504abcd519c048
Dmytro Kachanyuk <kachanyukdima@gmail.com>
2026-05-09 14:18:44 +0300

    handle errors in cli.py

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 import sys
 
 from src.engine.executor import Executor
+from src.exceptions import MicroSqlException
 from src.parser.query_parser import QueryParser
 from src.utils.csv_loader import CsvLoader
 from src.utils.output_formatter import OutputFormatter
@@ -13,17 +14,23 @@ def main():
 
     sql_file_path = sys.argv[1]
 
-    query_parser = QueryParser()
-    query = query_parser.parse(sql_file_path)
+    try:
+        query_parser = QueryParser()
+        query = query_parser.parse(sql_file_path)
 
-    csv_loader = CsvLoader()
-    table = csv_loader.load(query.table_name)
+        csv_loader = CsvLoader()
+        table = csv_loader.load(query.table_name)
 
-    executor = Executor()
-    result_rows = executor.execute(table, query)
+        executor = Executor()
+        result_rows = executor.execute(table, query)
 
-    formatter = OutputFormatter()
-    formatter.print_results(result_rows)
+        formatter = OutputFormatter()
+        formatter.print_results(result_rows)
+
+    except MicroSqlException as error:
+        error_type = type(error).__name__
+        print(f"Error: [{error_type}] - {error}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
